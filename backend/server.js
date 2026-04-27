67455776af4d739716bef14e6c9d71e2df7183e1
IT24102079 <it24102079@my.sliit.lk>
2026-04-27 22:33:42 +0530

    Cors issue resolved

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://wmt-lab-test-nu.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
